5c28fd0f6d1379006babf1fa6e8a8773f6099d13
Soham <sohamsawant092@gmail.com>
2026-07-07 15:24:02 +0530

    Refactor input fields for AI Hiring Prediction System

--- app.py
+++ app.py
@@ -1,26 +1,113 @@
+%%writefile app.py
+
 import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("Logistic_hiring_model.pkl")
-st.title("Ai Hiring Prediction System")
+# Load model
+model = joblib.load("logistic_hiring_model.pkl")
+
+st.title("AI Hiring Prediction System")
+
 candidate_id = st.number_input("Candidate ID", value=1)
+
 age = st.number_input("Age", 18, 70)
+
 gender = st.selectbox("Gender", ["Male", "Female"])
-education_level = st.selectbox("Education Level", ["Bachelor","Master","PhD"])
-university_tier = st.selectbox("University Tier", [1,2,3])
-years_experience = st.number_input("Years of Experience",0, 40)
-employment_gap_months = st.number_input("Employment Gap (Months)",0, 120) 
-technical_skill_score = st.slider("Technical Skill Score",0, 100)
-communication_score = st.slider("Communication Score",0, 100)
-aptitude_test_score = st.slider("Aptitude Test Score",0, 100)
-coding_test_score = st.slider("Coding Test Score",0, 100)
-project_count = st.number_input("Project Count",0, 50)
-github_activity_score = st.slider("GitHub Activity Score", 0, 100)
-certifications_count = st.number_input("Certifications",0, 20)
-expected_salary = st.number_input("Expected Salary")
-ai_resume_score = st.slider("Ai Resume Score",0, 100)
-ai_bias_score = st.slider("Ai Bias Score",0.0, 1.0)
+
+education_level = st.selectbox(
+    "Education Level",
+    ["Bachelor", "Master", "PhD"]
+)
+
+university_tier = st.selectbox(
+    "University Tier",
+    [1,2,3]
+)
+
+years_experience = st.number_input(
+    "Years of Experience",
+    0,
+    40
+)
+
+employment_gap_months = st.number_input(
+    "Employment Gap (Months)",
+    0,
+    120
+)
+
+technical_skill_score = st.slider(
+    "Technical Skill Score",
+    0,
+    100
+)
+
+communication_score = st.slider(
+    "Communication Score",
+    0,
+    100
+)
+
+aptitude_test_score = st.slider(
+    "Aptitude Test Score",
+    0,
+    100
+)
+
+coding_test_score = st.slider(
+    "Coding Test Score",
+    0,
+    100
+)
+
+project_count = st.number_input(
+    "Project Count",
+    0,
+    50
+)
+
+github_activity_score = st.slider(
+    "GitHub Activity Score",
+    0,
+    100
+)
+
+certifications_count = st.number_input(
+    "Certifications",
+    0,
+    20
+)
+
+expected_salary = st.number_input(
+    "Expected Salary"
+)
+
+ai_resume_score = st.slider(
+    "AI Resume Score",
+    0,
+    100
+)
+
+ai_bias_score = st.slider(
+    "AI Bias Score",
+    0.0,
+    1.0
+)
+
+# IMPORTANT:
+# Replace these values with the same encoding
+# you used while training.
+
+gender = 1 if gender=="Male" else 0
+
+education = {
+    "Bachelor":0,
+    "Master":1,
+    "PhD":2
+}
+
+education_level = education[education_level]
 
 if st.button("Predict"):
 
